02d3e6ad38642530b49f68f6d8e494cbe1c7780f
natasobl <natasobl@hotmail.com>
2023-05-05 17:59:13 -0400

    Migrating

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,17 @@
-const express = require('express');
+const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
 const cors = require("cors");
 const dotenv = require("dotenv");
 dotenv.config();
 const HTTP_PORT = process.env.PORT || 5000;
-const studentRoutes = require('./routes/student');
-const courseRoutes = require('./routes/course');
-const resultRoutes = require('./routes/result');
+const studentRoutes = require("./routes/student");
+const courseRoutes = require("./routes/course");
+const resultRoutes = require("./routes/result");
 
-require('./models/Student');
-require('./models/Course');
-require('./models/Result');
+require("./models/Student");
+require("./models/Course");
+require("./models/Result");
 
 mongoose.set("strictQuery", false);
 
@@ -19,12 +19,11 @@ app.use(express.json());
 app.use(cors());
 
 mongoose.connect(
-    'mongodb+srv://natasobl:cdtnrf2006@cluster0.pwvoqoy.mongodb.net/srms?retryWrites=true&w=majority',
-    { 
-        useNewUrlParser: true, 
-        useUnifiedTopology: true 
-    },
-   
+  "mongodb+srv://shyftlabs:7rail4K7Uts2Wfbi@cluster0.pja0kmn.mongodb.net/srms?retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
 );
 
 const db = mongoose.connection;
@@ -33,12 +32,10 @@ db.once("open", function () {
   console.log("Connected successfully");
 });
 
-
-app.use('/student', studentRoutes);
-app.use('/course', courseRoutes);
-app.use('/result', resultRoutes);
-
+app.use("/student", studentRoutes);
+app.use("/course", courseRoutes);
+app.use("/result", resultRoutes);
 
 app.listen(HTTP_PORT, () => {
-    console.log("Server is listening on port ", HTTP_PORT)
+  console.log("Server is listening on port ", HTTP_PORT);
 });
