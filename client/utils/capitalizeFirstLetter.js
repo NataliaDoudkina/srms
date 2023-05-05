410ef94f6e2b4c173aadd791b7fb6bcfafbde7d1
NataliaDoudkina <48611728+NataliaDoudkina@users.noreply.github.com>
2023-05-05 19:24:41 -0400

    Add utils

---
+++ client/utils/capitalizeFirstLetter.js
@@ -0,0 +1,10 @@
+const capitalizeFirstLetter = (value) => {
+  const words = value.split(" ");
+  for (let i = 0; i < words.length; i++) {
+    words[i] = words[i][0].toUpperCase() + words[i].substr(1);
+  }
+
+  return words.join(" ");
+};
+
+export default capitalizeFirstLetter;
