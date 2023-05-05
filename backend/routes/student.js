44b5962fe93bab1fc2fbcaa41f2277ec55e92b7d
NataliaDoudkina <48611728+NataliaDoudkina@users.noreply.github.com>
2023-05-05 18:09:22 -0400

    Delete backend directory

--- backend/routes/student.js
+++
@@ -1,9 +0,0 @@
-
-const express = require("express");
-const router = express.Router();
-const studentCtrl = require('../controllers/studentCtrl');
-
-router.get('/', studentCtrl.getAllStudents);
-router.post("/", studentCtrl.saveStudent);
-
-module.exports = router;
